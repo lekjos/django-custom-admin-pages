de0adf940da3d6d7b7392b4b8ae4eae7a943d0d9
lekjos <leif@leifkjos.com>
2023-11-12 09:10:03 -0800

    isort

--- test_proj/urls.py
+++ test_proj/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from test_app import views
 from another_test_app.views import AnotherExampleAdminView
+from test_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
