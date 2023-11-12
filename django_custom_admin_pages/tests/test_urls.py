93717b1d29881859273b74a3a74c518c890c0d50
lekjos <leif@leifkjos.com>
2023-11-12 09:15:15 -0800

    Re-add url

--- django_custom_admin_pages/tests/test_urls.py
+++ django_custom_admin_pages/tests/test_urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from test_proj.another_test_app.views import (
+    AnotherExampleAdminView,  # required for view to register
+)
+
 urlpatterns = [path("admin/", admin.site.urls)]
